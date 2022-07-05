24afacf87e9e1c1572f4a365e780a0c7d46838ae
Ramses <141248+R-VdP@users.noreply.github.com>
2022-07-05 11:58:17 +0200

    Make rsync ignore timestamps.

--- build_prod.sh
+++ build_prod.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 nix-build --arg production true
-rsync --archive --verbose --recursive --delete --chmod=u=rwX result/ docs/
+# Ignore time stamps since we will copy from the nix store
+rsync --archive --verbose --recursive --delete --ignore-times --chmod=u=rwX result/ docs/
 touch docs/.nojekyll
 
