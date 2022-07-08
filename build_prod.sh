2998d024140afb55440ce9b0024fbd88eec3cce7
Ramses <141248+R-VdP@users.noreply.github.com>
2022-07-08 17:28:24 +0200

    Prod site built by GH Actions.

--- build_prod.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-nix-build --arg production true
-# Ignore time stamps since we will copy from the nix store
-rsync --archive --verbose --recursive --delete --ignore-times --chmod=u=rwX result/ docs/
-touch docs/.nojekyll
-
