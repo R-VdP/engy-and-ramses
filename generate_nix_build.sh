c41a7377c69a4736262edeea7e5d7d537afa263f
Ramses <141248+R-VdP@users.noreply.github.com>
2022-07-11 12:03:05 +0200

    Improve generate_nix_build.sh

--- generate_nix_build.sh
+++ generate_nix_build.sh
@@ -1,5 +1,13 @@
-#! /usr/bin/env bash
+#! /usr/bin/env nix-shell
+#! nix-shell -p elm2nix -i bash
 
-elm2nix convert > elm-srcs.nix
-elm2nix snapshot > registry.dat
+repo_dir="$(dirname "$(realpath "${0}")")"
+
+cd "${repo_dir}"
+
+mkdir --parents "${repo_dir}/nix/"
+
+elm2nix convert > "${repo_dir}/nix/elm-srcs.nix"
+elm2nix snapshot
+mv ./registry.dat "${repo_dir}/nix/registry.dat"
 
