2e419562a2664d5c67dbaaec901aa0b569e30ab7
Ramses <141248+R-VdP@users.noreply.github.com>
2022-07-24 18:32:59 +0200

    Improve init script to be more resilient.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 function initApp() {
-  // The id of the element used to determine the viewport height
+  "use strict";
+
+  // The id of the element used to determine the viewport dimensions.
   const introElementId = "intro-full-viewport"
 
+  function getIntroElement() {
+    return document.getElementById(introElementId)
+  }
+
   function getInnerDimensions() {
-    const element = document.getElementById(introElementId)
+    const introElem = getIntroElement()
+    // If the intro element does not exist yet,
+    // we send the inner* dimensions instead.
     return {
-      width:  element ? element.clientWidth  : window.innerWidth,
-      height: element ? element.clientHeight : window.innerHeight
+      width:  introElem ? introElem.clientWidth  : window.innerWidth,
+      height: introElem ? introElem.clientHeight : window.innerHeight
     }
   }
 
@@ -14,41 +22,50 @@ function initApp() {
     elmApp.ports.receiveWindowSize.send(getInnerDimensions())
   }
 
-  let app = undefined
+  function go() {
+    let app = undefined
 
-  // We want to add a ResizeObserver once the element with
-  // id intro-full-viewport gets created.
-  // We will thus observe DOM changes until we find this element.
-  new MutationObserver((_mutationList, observer) => {
-    const introElem = document.getElementById(introElementId)
-    if (introElem) {
-      // We found the element, further events do not need to be handled,
-      // so we can disconnect this observer.
-      observer.disconnect()
-
-      // Every time the viewport size changes, we inform the Elm app about this.
-      new ResizeObserver(_entries => {
-        // If the app does not exist yet, we do nothing.
-        if (app) {
-          sendInnerDimensions(app)
-        }
-      }).observe(introElem)
+    function withApp(continuation) {
+      // If the app does not exist yet, we do nothing.
+      if (app) {
+        continuation(app)
+      }
     }
-  }).observe(document.body,
-             { attributes: false, childList: true, subtree: true })
 
-  // The MutationObserver is in place, we can launch our Elm app.
-  app = Elm.Main.init({
-    flags: {
-      introFullVpId: introElementId,
-      windowSize: getInnerDimensions()
-    }
-  })
+    // We want to add a ResizeObserver once the element with
+    // id intro-full-viewport gets created.
+    // We will thus observe DOM changes until we find this element.
+    new MutationObserver((_mutationList, observer) => {
+      const introElem = getIntroElement()
+      if (introElem) {
+        // We found the element, further events do not need to be handled,
+        // so we can disconnect this observer.
+        observer.disconnect()
+
+        // Every time the viewport size changes, we inform the Elm app about this.
+        // The observer will trigger a first time when observation starts.
+        // See: https://www.w3.org/TR/resize-observer/#intro
+        new ResizeObserver(_entries => {
+          withApp(sendInnerDimensions)
+        }).observe(introElem)
+      }
+    }).observe(document.body, { childList: true, subtree: true })
+
+    // The MutationObserver is in place, we can launch our Elm app.
+    app = Elm.Main.init({
+      flags: {
+        introFullVpId: introElementId,
+        windowSize: getInnerDimensions()
+      }
+    })
+  }
+
+  if (document.readyState === 'loading') {  // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', go)
+  } else {  // DOMContentLoaded has already fired
+    go()
+  }
 }
 
-if (document.readyState === 'loading') {  // Loading hasn't finished yet
-  document.addEventListener('DOMContentLoaded', initApp)
-} else {  // `DOMContentLoaded` has already fired
-  initApp()
-}
+initApp()
 
